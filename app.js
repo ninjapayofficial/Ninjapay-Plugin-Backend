865f3e95b5d8d481ffe1da2fb4294f05050e240a
dev <dev@ninjapay.me>
2024-12-06 04:40:24 +0000

    removed firebase hardcoded values fro html

--- app.js
+++ app.js
@@ -40,6 +40,28 @@ const sequelize = new Sequelize(
   },
 );
 
+// Route to serve Firebase config as a JavaScript file
+app.get('/firebase-config.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+
+  res.send(`
+    // firebase-config.js
+    const firebaseConfig = ${JSON.stringify(firebaseConfig)};
+    firebase.initializeApp(firebaseConfig);
+    const db = firebase.firestore();
+  `);
+});
+
 // // Alternatively, you can use the connection URI provided:
 // const sequelize = new Sequelize(process.env.DATABASE_URL, {
 //   dialect: 'postgres',
