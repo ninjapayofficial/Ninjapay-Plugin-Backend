0bd65957e8a02993d4de93b8c8b117f3f8810099
dev <dev@ninjapay.me>
2024-12-08 12:58:32 +0000

    swagger api fix... working...added auth also

--- routes/pluginRoutes.js
+++ routes/pluginRoutes.js
@@ -28,26 +28,6 @@ const { sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
 
-// // Database Connection
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: process.env.DB_HOST,
-//     dialect: process.env.DB_DIALECT || "postgres",
-//     port: process.env.DB_PORT || 5432,
-//     dialectOptions: {
-//       ssl:
-//         process.env.DB_SSL === "true"
-//           ? {
-//               require: true,
-//               rejectUnauthorized: false,
-//             }
-//           : false,
-//     },
-//   },
-// );
 
 /**
  * @swagger
