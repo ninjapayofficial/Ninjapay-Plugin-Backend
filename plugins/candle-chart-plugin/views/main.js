e1392ceb93277d1b04d29233e814d572b2a55323
dev <dev@ninjapay.me>
2024-12-18 06:15:34 +0000

    chart styling

--- plugins/candle-chart-plugin/views/main.js
+++ plugins/candle-chart-plugin/views/main.js
@@ -3,7 +3,6 @@
 /* eslint-disable no-unused-vars */
 /* global LightweightCharts */
 // Ensure the file name here matches the one referenced in your HTML script tag.
-// Assuming your HTML is `<script src="main.js"></script>`, call this file `main.js`.
 document.addEventListener('DOMContentLoaded', () => {
     const { createChart } = LightweightCharts;
     const chartContainer = document.getElementById('chart-container');
